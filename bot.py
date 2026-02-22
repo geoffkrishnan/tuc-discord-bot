93f16eb462e6be778069eb4d0454473f97c3f26f
Geoff Krishnan <geoffrykrishnan@gmail.com>
2026-02-22 01:52:08 -0600

    feat: add weekly completion log post scheduling logic

--- bot.py
+++ bot.py
@@ -1,21 +1,47 @@
 import discord
-from discord.ext import tasks
 import os
 from dotenv import load_dotenv
+import datetime
+from datetime import timedelta
+import asyncio
 
 
 load_dotenv()
 bot = discord.Bot()
+
+current_time = datetime.datetime.now()
+COMPLETION_LOG_CHANNEL_ID = 1475023542784626711
 
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
+    try:
+        with open("last_post_date.txt", "r") as f:
+            last_post_date = f.read()
+            last_post_date = datetime.datetime.fromisoformat(last_post_date)
+            difference = current_time - last_post_date
+            if difference.days >= 7:
+                await create_completion_log(current_time)
+            else:
+                remaining = timedelta(days=7) - difference
+                await asyncio.sleep(remaining.total_seconds())
+                await create_completion_log(current_time)
+    except FileNotFoundError:
+        await create_completion_log(current_time)
 
-@bot.slash_command(name="hello", description="Say hello to the bot")
-async def hello(ctx: discord.ApplicationContext):
-    await ctx.respond("Hey!")
+
+async def create_completion_log(time):
+    start = time
+    end = start + timedelta(days=7)
+    title = f"Completion Log {start.strftime('%-m/%-d/%-y')} - {end.strftime('%-m/%-d/%-y')}"
+    with open("post.md", "r") as f:
+        content = f.read()
+    channel = await bot.fetch_channel(COMPLETION_LOG_CHANNEL_ID)
+    await channel.create_thread(name=title, content=content)
+    with open("last_post_date.txt", "w") as f:
+        f.write(time.isoformat())
 
 
 bot.run(os.getenv("TOKEN"))
---
+++ post.md
@@ -0,0 +1,17 @@
+# Completion Logs
+Every week, a completion log will be created. Under these posts, you can share what you have completed and what you are working towards. Consistently filling these logs helps create accountability. Use these a means to track your progress towards goals and help motivate fellow community members.
+
+# What should my responses look like?
+There is no set format for completion log responses. However, you should keep your log to a single message within each post for organization purposes. Below are examples of what you can include:
+
+# Completion Log 1/25/26 - 1/31/26
+- Completed Figma design file for project ...
+- Completed several simple learning projects: ...
+- Working on x in order to accomplish y.
+- Studied Flexbox, Box Model for HTML
+
+# What counts as a notable completion?
+The criteria for a notable completion is dependent on the person posting. You should gauge whether something you have done is notable. For some, this may be simple studies. For others, this could be completed components for projects and/or deadlines fulfilled.
+
+# What if I complete something outside of the period?
+If you completed something notable and there's no related post, feel free to ping @Akryllia to create a post for the time period. You may also include it in the most recent completion log with an indicator of the time period it is intended for.
